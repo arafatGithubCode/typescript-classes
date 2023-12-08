8145ae7c6aab8309173cddf32159412d9f36e550
arafatGithubCode <md489288@gmail.com>
2023-12-08 16:18:52 +0600

    first ts program

---
+++ C-2  First Typescript program/index.ts
@@ -0,0 +1,11 @@
+//tsc index.ts -> index.js -> node index.js
+
+//tsc index.ts --watch
+
+const addNumber = (num1: number, num2: number) => {
+  console.log(num1 + num2);
+};
+addNumber(5, 5);
+
+// addNumber(5, "5");
+// addNumber(5, );
---
+++ tutorial/index.js
@@ -0,0 +1,8 @@
+//tsc index.ts -> index.js -> node index.js
+//tsc index.ts --watch
+var addNumber = function (num1, num2) {
+    console.log(num1 + num2);
+};
+addNumber(5, 5);
+// addNumber(5, "5");
+// addNumber(5, );
