2c7b8c3e8314b2e33027d9132492df75ef234c41
arafatGithubCode <md489288@gmail.com>
2023-12-09 14:31:30 +0600

    tuple-data-type

---
+++ C-6 Tuple-data-type/index.js
@@ -0,0 +1,7 @@
+// let me = [38, "Arafat"]; //38=key, "Arafat"=key-value
+var user;
+user = [38, "Arafat"];
+user.push(39, "MOn");
+user.pop();
+console.log(user);
+console.log(user[0], user[1]);
---
+++ C-6 Tuple-data-type/index.ts
@@ -0,0 +1,9 @@
+// let me = [38, "Arafat"]; //38=key, "Arafat"=key-value
+
+let user: [number, string];
+user = [38, "Arafat"];
+user.push(39, "MOn");
+user.pop();
+
+console.log(user);
+console.log(user[0], user[1]);
